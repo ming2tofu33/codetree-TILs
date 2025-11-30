1afbf5295bd536864ba687c4c7d978a4b204cf29
amy <ming2tofu33@gmail.com>
2025-11-30 23:20:54 +0900

    Title: 숫자 빠르게 찾기, Time: 190ms, Memory: 25MB, Status: Passed - Codetree

--- 251130/숫자 빠르게 찾기/find-number-fast.py
+++ 251130/숫자 빠르게 찾기/find-number-fast.py
@@ -1,19 +1,16 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 queries = [int(input()) for _ in range(m)]
 
 # Please write your code here.
 def binary_search(arr, target):
-    left, right = 0, len(arr)
+    left, right = 0, len(arr) - 1
 
     while left <= right:
         mid = (left + right) // 2
 
         if arr[mid] == target:
-            return mid
+            return mid + 1
         elif arr[mid] < target:
             left = mid + 1
         else:
@@ -22,5 +19,4 @@ def binary_search(arr, target):
     return -1
 
 for q in queries:
-    index = binary_search(arr, q)
-    print(-1 if index < 0 else index + 1)
+    print(binary_search(arr, q))
