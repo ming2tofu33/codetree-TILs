a17c51860a804eaab946a0e4f0be33c9bcad4859
amy <ming2tofu33@gmail.com>
2025-11-30 23:12:40 +0900

    Title: 숫자 빠르게 찾기, Time: 165ms, Memory: 24MB, Status: Runtime Error - Codetree

---
+++ 251130/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+# Please write your code here.
+def binary_search(arr, target):
+    left, right = 0, len(arr)
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid + 1
+        elif arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+    
+    return -1
+
+for q in queries:
+    print(binary_search(arr, q))
