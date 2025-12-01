29e4ed6e0d8202a765f33c8c43dabbff32d9f5fc
amy <ming2tofu33@gmail.com>
2025-12-01 15:39:42 +0900

    Title: 가장 먼저 나오는 숫자, Time: 125ms, Memory: 33MB, Status: Passed - Codetree

--- 251201/가장 먼저 나오는 숫자/first-appear-number.py
+++ 251201/가장 먼저 나오는 숫자/first-appear-number.py
@@ -21,3 +21,28 @@ def bisect_left(target):
 
 for q in query:
     print(bisect_left(q))
+
+
+# n, m = tuple(map(int, input().split()))
+# arr = [0] + list(map(int, input().split()))
+# querries = list(map(int, input().split()))
+
+# def lower_bound(target):
+#     left, right = 1, n
+#     min_idx = n + 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if arr[mid] >= target:
+#             right = mid - 1
+#             min_idx = min(min_idx, mid)
+#         else:
+#             left = mid + 1
+
+#     return min_idx
+
+# for querry in querries:
+#     lo = lower_bound(querry)
+#     if lo <= n and arr[lo] == querry:
+#         print(lo)
+#     else:
+#         print(-1)
