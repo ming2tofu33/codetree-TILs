8bbc90812501b2691fd35a703865eff5f0e7212c
amy <ming2tofu33@gmail.com>
2025-12-01 23:53:15 +0900

    Title: 가장 짧은 부분합, Time: 58ms, Memory: 23MB, Status: Wrong Answer - Codetree

--- 251201/가장 짧은 부분합/shortest-subtotal.py
+++ 251201/가장 짧은 부분합/shortest-subtotal.py
@@ -4,13 +4,20 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 INF = float('inf')
 min_len = INF
-i, j = 0, 1
 
-while j < n:
-    if sum(arr[i: j]) >= s:
-        min_len = min(min_len, len(arr[i: j]))
-        i += 1
-    else:
+i = 0
+j = 0
+curr_sum = 0 
+
+while True:
+    if curr_sum >= s:
+        min_len = min(min_len, j - i)
+        curr_sum -= arr[i]
+        i += 1 
+    elif j < n:
+        curr_sum += arr[j]
         j += 1
+    else:
+        break
 
-print(min_len)
+print(0 if min_len == INF else min_len)
