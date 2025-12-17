b2e2f739c0ef9d4d205f32b4e0b6bf089b393d3d
amy <ming2tofu33@gmail.com>
2025-12-18 01:46:53 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 68ms, Memory: 19MB, Status: Passed - Codetree

---
+++ 251218/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(N))
