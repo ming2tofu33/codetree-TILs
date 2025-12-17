fa21a71b469be36f1267e5280c1c7ede49742c0d
amy <ming2tofu33@gmail.com>
2025-12-18 00:56:30 +0900

    Title: 1이 되는 순간까지, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251218/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 251218/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -2,17 +2,28 @@ N = int(input())
 
 # Please write your code here.
 
-cnt = 0
-
 def f(n):
-    global cnt
     if n == 1:
-        return cnt
-    if n % 2:
-        cnt += 1
-        return f(n // 3)
+        return 0
+        
+    if n % 2 == 0:
+        return f(n // 2) + 1
     else:
-        cnt += 1
-        return f(n // 2)
+        return f(n // 3) + 1
 
 print(f(N))
+
+# cnt = 0
+
+# def f(n):
+#     global cnt
+#     if n == 1:
+#         return cnt
+#     if n % 2:
+#         cnt += 1
+#         return f(n // 3)
+#     else:
+#         cnt += 1
+#         return f(n // 2)
+
+# print(f(N))
