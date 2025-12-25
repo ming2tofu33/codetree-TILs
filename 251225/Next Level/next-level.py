f06e492e414a87956f722b89e45d7ec0fbb6b432
amy <ming2tofu33@gmail.com>
2025-12-25 22:45:01 +0900

    Title: Next Level, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251225/Next Level/next-level.py
@@ -0,0 +1,17 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+# Please write your code here.
+
+class Info:
+    def __init__(self, user_id='codetree', level=10):
+        self.user_id = user_id
+        self.level = level
+    
+    def __str__(self):
+        return f'user {self.user_id} lv {self.level}'
+
+user1 = Info()
+print(user1)
+user2 = Info(user2_id, user2_level)
+print(user2)
