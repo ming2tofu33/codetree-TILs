8ab9b21f3753b19f303c400f3fe2e728d9a04014
amy <ming2tofu33@gmail.com>
2026-01-03 14:24:08 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260103/DateTime to DateTime/datetime-to-datetime.py
+++ 260103/DateTime to DateTime/datetime-to-datetime.py
@@ -24,3 +24,13 @@ while True:
             hour = 0
 
 print(elapsed_time)
+
+
+# a, b, c = tuple(map(int, input().split()))
+
+# diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
+
+# if diff < 0:
+#     print(-1)
+# else:
+#     print(diff)
