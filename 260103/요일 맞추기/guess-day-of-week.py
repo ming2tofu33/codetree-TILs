8f88419b8196d9f7059e840e2aa2ada6aa66555b
amy <ming2tofu33@gmail.com>
2026-01-03 17:04:01 +0900

    Title: 요일 맞추기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260103/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,30 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+
+months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+date = [m1, d1]
+days = 0
+
+while True:
+    if date[0] == m2 and date[1] == d2:
+        break
+    
+    if (m1, d1) > (m2, d2):
+        date[1] -= 1
+        days -= 1
+        
+        if date[1] < 1:
+            date[0] -= 1
+            date[1] = months[date[0]]
+    
+    elif (m1, d1) < (m2, d2):    
+        date[1] += 1
+        days += 1
+        
+        if date[1] > months[date[0]]:
+            date[0] += 1
+            date[1] = 1
+
+print(day[days % 7])
