1e6148f2f55f8f340374b3fb16ab502e99220afd
amy <ming2tofu33@gmail.com>
2026-01-03 14:21:16 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260103/DateTime to DateTime/datetime-to-datetime.py
+++ 260103/DateTime to DateTime/datetime-to-datetime.py
@@ -2,15 +2,25 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 
-d, h, m  = 11, 11, 11
-    elapsed_days += 1
-    day += 1
-
-    if day > months[month]:
-        month += 1
-        day = 1
-
-print(elapsed_days + 1)
+day, hour, minute  = 11, 11, 11
+elapsed_time = 0
 
 while True:
-    if d == a and h == b and m == c:
+    if (a, b, c) < (day, hour, minute):
+        elapsed_time = -1
+        break
+
+    if day == a and hour == b and minute == c:
+        break
+    
+    elapsed_time += 1
+    minute += 1
+
+    if minute == 60:
+        hour += 1
+        minute = 0
+        if hour == 24:
+            day += 1
+            hour = 0
+
+print(elapsed_time)
